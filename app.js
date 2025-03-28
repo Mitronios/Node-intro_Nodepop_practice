52d1be577982acb64087f841e55f9dd82437dbb0
Miguel Angel Gonzales <mitronios@gmail.com>
2025-03-28 06:45:52 -0400

    Feat: add mongoose config and connect to mongodb

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
 import ejs from "ejs";
+import connectMongoose from "./lib/connectMongoose.js";
 import homeRoutes from "./routes/home.js";
 
+//Mongoose connect
+await connectMongoose();
+
+//Create express app
 const app = express();
 
 //Configs
@@ -36,7 +41,7 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
 
-  //Validations
+  //General Validation
   if (err.array) {
     err.message =
       "Invalid request: " +
