d8aa245aeb171caaff325fc9e0a29683b8fd3ea2
Miguel Angel Gonzales <mitronios@gmail.com>
2025-03-29 19:46:28 -0400

    Fix: change userID for User Name

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,12 +1,14 @@
 import Product from "../models/Product.js";
+import User from "../models/User.js";
 
 export async function index(req, res, next) {
   try {
     if (!req.session.userID) {
-      res.render("home", { products: [] });
+      res.render("home", { products: [], userName: null });
     } else {
       const products = await Product.find({ owner: req.session.userID });
-      res.render("home", { products: products });
+      const user = await User.findById(req.session.userID);
+      res.render("home", { products: products, userName: user.name });
     }
   } catch (error) {
     next(error);
