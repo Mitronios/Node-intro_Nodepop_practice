dc0cf87e88e5fa059b9afb2b91edafb62f30e2a4
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-06-28 14:37:59 -0400

    feature: add cookie-parser to handle cookies. language-toggler is functional.

--- controllers/langController.js
+++ controllers/langController.js
@@ -1,8 +1,9 @@
 export const changeLang = (req, res, next) => {
   const thirtyDays = 1000 * 60 * 60 * 24 * 30;
   const locale = req.params.locale;
-  res.cookie('nodeap-locale', locale, {
+  res.cookie('nodeapp-locale', locale, {
     maxAge: thirtyDays,
   });
-  res.redirect('back');
+  const redirectTo = req.get('Referrer') || '/';
+  res.redirect(redirectTo);
 };
