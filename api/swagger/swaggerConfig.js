e1d0f4781704225b761f8a7a44c04d559e842748
Miguel Angel Gonzales Melgar <mitronios@gmail.com>
2025-06-28 21:53:27 -0400

    feature: add swaggerConfig and complete API docs.

---
+++ api/swagger/swaggerConfig.js
@@ -0,0 +1,26 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Nodepop API',
+      version: '1.0.0',
+      description: 'API for products management',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/api/products',
+        description: 'Local server',
+      },
+    ],
+  },
+  apis: [
+    './api/routes/*.js',
+    './api/apiProductController.js',
+    './api/swagger/swaggerComponents.js',
+    './api/swagger/docs/*.js',
+  ],
+};
+
+export const swaggerSpec = swaggerJSDoc(options);
